101ba2eab7b78272da21e9c39567202c1206df9a
Yury Brichev <ybarichev@yandex.ru>
2024-11-20 17:35:54 +0300

    multitag

--- build_to_dockerhub.sh
+++ build_to_dockerhub.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build  -t ybarichev/my_uav:pure -f ./dockerfiles/Dockerfile_pure .
+docker build -t my-ros-humble:pure -f ./dockerfiles/Dockerfile_pure .
+docker build -t my-ros-humble:quill5 -f ./dockerfiles/Dockerfile_quill5 .
+docker build -t my-ros-humble:easy_profiler -f ./dockerfiles/Dockerfile_easy_profiler .
+docker build -t my-ros-humble:debug -f ./dockerfiles/Dockerfile_debug .
